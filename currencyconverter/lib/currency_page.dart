14b52163787f00c74c729601629d83cef4ef34ef
MeitanteiAshour <50237142+MeitanteiAshour@users.noreply.github.com>
2020-04-10 20:16:32 +0200

    finished currency converter project

--- currencyconverter/lib/currency_page.dart
+++ currencyconverter/lib/currency_page.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class CurrencyPage extends StatefulWidget {
   @override
@@ -6,6 +9,9 @@ class CurrencyPage extends StatefulWidget {
 }
 
 class _CurrencyPageState extends State<CurrencyPage> {
+  var firstCurrency = "USD";
+  var secondCurrency = "CAD";
+  var addedValue = 1.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,23 +22,71 @@ class _CurrencyPageState extends State<CurrencyPage> {
             top: 0,
             left: 0,
             child: Container(
-              child: Container(
-                width:200,
-                height: 200,
-                child: Center(child: TextField()),
-              ),
               width: MediaQuery.of(context).size.width,
               height: 500,
+              child: Container(
+                width: 200,
+                height: 200,
+                child: Center(
+                  child: Wrap(
+                    children: [
+                      Container(
+                        width: 130,
+                        child: TextField(
+                          onChanged: (String value) {
+                            setState(() {
+                              addedValue = double.parse(value);
+                            });
+                          },
+                          style: TextStyle(color: Colors.white, fontSize: 40),
+                        ),
+                      ),
+                      FutureBuilder(
+                        future: _getData(),
+                        builder:
+                            (BuildContext context, AsyncSnapshot snapshot) {
+                          return Container(
+                            child: snapshot.hasData
+                                ? DropdownButton(
+                                    items: snapshot.data['rates'].keys
+                                        .map<DropdownMenuItem<String>>(
+                                          (String value) =>
+                                              DropdownMenuItem<String>(
+                                            child: Text('$value'),
+                                            value: value,
+                                          ),
+                                        )
+                                        .toList(),
+                                    onChanged: (String value) {
+                                      setState(
+                                        () {
+                                          firstCurrency = value;
+                                        },
+                                      );
+                                    },
+                                    value: firstCurrency,
+                                  )
+                                : SizedBox(),
+                          );
+                        },
+                      )
+                    ],
+                  ),
+                ),
+              ),
               decoration: BoxDecoration(
                 gradient: LinearGradient(
-                  colors: [Colors.white, Colors.orange, Colors.deepOrange],
+                  colors: [
+                    Colors.white70,
+                    Color(0xffFF5250),
+                    Color(0xffFF5270)
+                  ],
                   begin: Alignment.bottomLeft,
                   end: Alignment.topRight,
                 ),
               ),
             ),
           ),
-
           //lower part
           Positioned(
             top: 430,
@@ -50,14 +104,88 @@ class _CurrencyPageState extends State<CurrencyPage> {
               height: 50,
               decoration: BoxDecoration(
                   color: Colors.white, borderRadius: BorderRadius.circular(10)),
-              child: Icon(
-                Icons.arrow_upward,
+              child: IconButton(
+                icon: Icon(Icons.arrow_upward),
+                onPressed: () {
+                  setState(
+                    () {
+                      var x;
+                      x = firstCurrency;
+                      firstCurrency = secondCurrency;
+                      secondCurrency = x;
+                    },
+                  );
+                },
                 color: Colors.pinkAccent,
               ),
             ),
           ),
+          Positioned(
+            bottom: 160,
+            left: 100,
+            child: FutureBuilder(
+              future: _fetchCurrency(firstCurrency, secondCurrency),
+              builder: (context, snapshot) => snapshot.hasData
+                  ? Center(
+                      child: Text(
+                        '${(snapshot.data['rates']['$firstCurrency'] * addedValue).toStringAsFixed(1)}',
+                        style: TextStyle(
+                          fontSize: 40,
+                          color: Color(0xffff5250),
+                        ),
+                      ),
+                    )
+                  : CircularProgressIndicator(),
+            ),
+          ),
+          Positioned(
+            right: 40,
+            bottom: 200,
+            child: FutureBuilder(
+              future: _getData(),
+              builder: (context, snapshot) {
+                return snapshot.hasData
+                    ? Container(
+                        child: DropdownButton(
+                          items: snapshot.data['rates'].keys
+                              .map<DropdownMenuItem<String>>(
+                                (element) => DropdownMenuItem<String>(
+                                  child: Text('$element'),
+                                  value: element,
+                                ),
+                              )
+                              .toList(),
+                          onChanged: (String value) {
+                            setState(
+                              () {
+                                secondCurrency = value;
+                              },
+                            );
+                          },
+                          value: secondCurrency,
+                        ),
+                      )
+                    : CircularProgressIndicator();
+              },
+            ),
+          )
         ],
       ),
     );
+  }
+
+  _getData() async {
+    var url = "https://api.exchangeratesapi.io/latest";
+    final response = await http.get(url);
+    var decoded = jsonDecode(response.body);
+    return decoded;
+  }
+
+  _fetchCurrency(firstCurrency, secondsCurrency) async {
+    var url =
+        "https://api.exchangeratesapi.io/latest?symbols=$firstCurrency,$secondsCurrency";
+    final response = await http.get(url);
+    var decoded = jsonDecode(response.body);
+    return decoded;
   }
 }
